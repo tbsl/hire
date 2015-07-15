2d217a23770cdad4866c1f9a446c18d8630e2127
unknown <Simmi.Jaiswal@NODTBSD180AG.timesgroup.com>
2015-07-15 15:17:28 +0530

    15july:wq

--- src/hireSideAutomation/SearchResultPage.java
+++ src/hireSideAutomation/SearchResultPage.java
@@ -40,15 +40,16 @@ public class SearchResultPage extends hireBaseFunctions
 		 hireBaseFunctions.startup();
 		
 		 environment();
-		 EmployerLogin(EmploginID, EmpPWD);
+		
 		
 	}
 	
-	
-	@Test(priority =1)
+	@SuppressWarnings("deprecation")
+	@Test(priority=10)
 	public void SendJobasEmail() throws Exception
 	     {
-		
+		   
+		   EmployerLogin(EmploginID, EmpPWD);
 		   // driver.findElement(By.cssSelector("#maintab_searchCand > a > b")).click();
 		    driver.findElement(By.xpath("//li[@id='maintab_searchCand']//a")).click();
 		    
@@ -59,7 +60,6 @@ public class SearchResultPage extends hireBaseFunctions
 			driver.findElement(By.id("token-input-txtKeywordLeast")).clear();
 			
 			//driver.findElement(By.id("token-input-txtKeywordLeast")).sendKeys( "\"jaiswal.simmi29@gmail.com\"" );
-			
 			driver.findElement(By.id("token-input-txtKeywordLeast")).sendKeys("java");
 			
 			WebElement desg1=driver.findElement(By.id("token-input-txtKeywordLeast"));
@@ -120,12 +120,12 @@ public class SearchResultPage extends hireBaseFunctions
 			
 	}	
 	
-	
-    @Test(priority =2)
+    @Test(priority=2)
 	public void SendEmail()throws Exception
-	{    	
+	{
     	
-    	 //EmployerLogin(EmploginID, EmpPWD);
+    	
+    	 EmployerLogin(EmploginID, EmpPWD);
     	 
 		  Thread.sleep(1000);
 		  driver.findElement(By.xpath("//li[@id='maintab_searchCand']//a")).click();
@@ -180,9 +180,75 @@ public class SearchResultPage extends hireBaseFunctions
 				driver.findElement(By.xpath("//input[@class='submit_btn2' and @name='cancel']")).click();
 			} 
 		  
+		  
+		
+	}
+    
+    @Test(priority=1)
+	public void ContactBySms()throws Exception
+	{
+    	 Calendar currentDate1 = Calendar.getInstance();
+         SimpleDateFormat formatter1= new SimpleDateFormat("ddMMyyyy_hhmmss");
+         String getDate = formatter1.format(currentDate1.getTime());
+         String tmpname = "template" + getDate ;
+         System.out.println(tmpname);
+    	
+    	 EmployerLogin(EmploginID, EmpPWD);
+    	 
+		  Thread.sleep(1000);
+		  driver.findElement(By.xpath("//li[@id='maintab_searchCand']//a")).click();
+		driver.findElement(By.linkText("Quick Search")).click();
+		
+		driver.findElement(By.id("token-input-txtKeywordLeast")).click();
+		driver.findElement(By.id("token-input-txtKeywordLeast")).clear();
+		
+		//driver.findElement(By.id("token-input-txtKeywordLeast")).sendKeys( "\"jaiswal.simmi29@gmail.com\"" );
+		driver.findElement(By.id("token-input-txtKeywordLeast")).sendKeys("java");
+		
+		WebElement desg1=driver.findElement(By.id("token-input-txtKeywordLeast"));
+		Actions ajaxM1=new Actions(driver);
+		ajaxM1.moveToElement(desg1).build().perform();
+		
+		driver.findElement(By.xpath("//input[@class='blue-btn' and @type='submit']")).click();
+		//driver.findElement(By.xpath("//input[contains(@id,'chk')]")).click();
+		
+		List<WebElement> checkBoxes=driver.findElements(By.className("brdBlu"));
+		
+		for(int i=2; i<=2;i++)
+		{
+            checkBoxes.get(i).click();
+			
+        }
+		
+		  
+		  driver.findElement(By.linkText("Contact by SMS")).click();
+		  driver.switchTo().frame("GB_frame1");
+		  driver.switchTo().frame("GB_frame");
+		  
+		  driver.findElement(By.xpath("//ul[@id='tabID']//a[@class='createTemp']")).click();
+		  driver.findElement(By.id("templateName")).click();
+		  driver.findElement(By.id("templateName")).sendKeys(tmpname);
+		  driver.findElement(By.id("templateMessage")).click();
+		  driver.findElement(By.id("templateMessage")).sendKeys("Hi this is test message");
+		  driver.findElement(By.id("sendNow")).click();
+		  driver.findElement(By.id("submit1")).click();
+		  
+		  try
+			{
+				assertTrue(driver.getPageSource().contains("SMS are successfully delivered."));
+				System.out.println("SMS successfully sent");
+				driver.findElement(By.xpath("//input[@class='submit_btn' and @name='cancel']")).click();
+				
+			}
+			catch(Throwable e)
+			{
+				System.out.println("SMS not sent");
+				driver.findElement(By.xpath("//input[@class='submit_btn' and @name='cancel']")).click();
+			} 
+		  driver.findElement(By.linkText("Logout")).click();
 	}
 	
-	@Test(priority =3)
+	@Test(priority=3)
 	public void DeleteJob() throws Exception
 	{
 		//EmployerLogin(c.getProperty("HireQCLoginid"), c.getProperty("HireQCPwd"),c.getProperty("HireQCUrl"));
@@ -196,15 +262,34 @@ public class SearchResultPage extends hireBaseFunctions
 		Alert alert = driver.switchTo().alert();
 	    alert.accept();
 	    String deletejobtext="The Job ID entered is not posted by you or shared to you.Please provide a valid Job ID.";
-	    
+	    Thread.sleep(2000);
 	    driver.findElement(By.linkText("Logout")).click();
 		
 	}
 	
 	@Test (priority=4)
-	public static void Shortlist() throws IOException, InterruptedException {
-		hireBaseFunctions.Search();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	public void Shortlist() throws Exception 
+	    {
+		
+		EmployerLogin(EmploginID, EmpPWD);
+   	 
+		  Thread.sleep(1000);
+		  driver.findElement(By.xpath("//li[@id='maintab_searchCand']//a")).click();
+		driver.findElement(By.linkText("Quick Search")).click();
+		
+		driver.findElement(By.id("token-input-txtKeywordLeast")).click();
+		driver.findElement(By.id("token-input-txtKeywordLeast")).clear();
+		
+		//driver.findElement(By.id("token-input-txtKeywordLeast")).sendKeys( "\"jaiswal.simmi29@gmail.com\"" );
+		driver.findElement(By.id("token-input-txtKeywordLeast")).sendKeys("java");
+		
+		WebElement desg1=driver.findElement(By.id("token-input-txtKeywordLeast"));
+		Actions ajaxM1=new Actions(driver);
+		ajaxM1.moveToElement(desg1).build().perform();
+		
+		driver.findElement(By.xpath("//input[@class='blue-btn' and @type='submit']")).click();
+		
+		
 		driver.findElement(By.xpath(p.getProperty("d_chkbx"))).click();
 		driver.findElement(By.linkText(p.getProperty("d_shortlst"))).click();
 		driver.switchTo().frame(p.getProperty("d_frm1"));
@@ -220,25 +305,29 @@ public class SearchResultPage extends hireBaseFunctions
 		int i =links.size();
 		for (int j=1;j<=i;j++)
 		{
-			Thread.sleep(5000);
 			String t1 = driver.findElement((By.id("folderId"+j))).getAttribute("value");
 			
 			if(t1.contains(timeStamp))
-			{
+			{   
+				Thread.sleep(5000);
 				driver.findElement(By.id("folderId"+j)).click();
 				driver.findElement(By.xpath(p.getProperty("d_shrtclick"))).click();
 				String expected = "The selected resume(s) have been successfully saved. You may visit folder to see the resume.";
 				String actual=driver.findElement(By.xpath(p.getProperty("d_shrtlstmsg"))).getText();
 				Assert.assertEquals(actual, expected);
-				
+				driver.findElement(By.xpath("//input[@class='submit_btn' and @name='cancel']")).click();
 				break;
 			}
 		}
+		 Thread.sleep(2000);
+		 driver.findElement(By.linkText("Logout")).click();
 		
 	}
 	
-	@Test (priority=4)
-	public static void AddComment() throws IOException, InterruptedException {
+	@Test (priority=5)
+	public void AddComment() throws Exception 
+	{   
+		EmployerLogin(EmploginID, EmpPWD);
 		hireBaseFunctions.Search();
 		WebElement ele = driver.findElement(By.linkText(p.getProperty("d_Addcmnt")));
 		String val = ele.findElement(By.xpath("..")).getAttribute("id");
@@ -259,12 +348,15 @@ public class SearchResultPage extends hireBaseFunctions
 		String expected =timeStamp;
 		System.out.println("actual "+actual);
 		System.out.println("expected "+expected);
-		Assert.assertEquals(actual, expected);		
+		Assert.assertEquals(actual, expected);	
+		
+		driver.findElement(By.linkText("Logout")).click();
 	}
 	
-	
-	@Test (priority=5)
-	public static void SavewithON() throws IOException, InterruptedException {
+	@Test (priority=6)
+	public void SavewithON() throws Exception 
+	 {
+		EmployerLogin(EmploginID, EmpPWD);
 		hireBaseFunctions.Search();
 		driver.findElement(By.linkText(p.getProperty("d_savelink"))).click();
 		driver.switchTo().frame(p.getProperty("d_frm1"));
@@ -277,29 +369,39 @@ public class SearchResultPage extends hireBaseFunctions
 		driver.findElement(By.xpath(p.getProperty("d_srchsave"))).click();
 		String actual=driver.findElement(By.xpath(p.getProperty("d_srchsavmsg"))).getText();
 		String expected="Your search has successfully been saved."; 
-		Assert.assertEquals(actual, expected);		
+		Assert.assertEquals(actual, expected);	
+		driver.findElement(By.xpath("//input[@class='submit' and @value='Close']")).click();
+		
+		driver.findElement(By.linkText("Logout")).click();
 	}
 	
-	
-	@Test (priority=6)
-	public static void SavewithOFF() throws IOException, InterruptedException {
+	@Test (priority=7)
+	public void SavewithOFF() throws Exception {
+		
+		EmployerLogin(EmploginID, EmpPWD);
 		hireBaseFunctions.Search();
 		driver.findElement(By.linkText(p.getProperty("d_savelink"))).click();
 		driver.switchTo().frame(p.getProperty("d_frm1"));
 		driver.switchTo().frame(p.getProperty("d_frm"));
 		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
 		driver.findElement(By.id(p.getProperty("d_savsrchname"))).sendKeys(timeStamp);
-		driver.findElement(By.id(p.getProperty("d_emailalteroff"))).click();
+		//driver.findElement(By.id(p.getProperty("d_emailalteroff"))).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath(".//*[@id='emailAlertStatus2']")).click();
 		driver.findElement(By.xpath(p.getProperty("d_alrtfreq"))).click();
 		driver.findElement(By.xpath(p.getProperty("d_srchsave"))).click();
 		String actual=driver.findElement(By.xpath(p.getProperty("d_srchsavmsg"))).getText();
 		String expected="Your search has successfully been saved."; 
-		Assert.assertEquals(actual, expected);		
+		Assert.assertEquals(actual, expected);	
+		driver.findElement(By.xpath("//input[@class='submit' and @value='Close']")).click();
+		
+		driver.findElement(By.linkText("Logout")).click();
 	}
 	
-	
-	@Test (priority=7)
-	public static void viewResume() throws IOException, InterruptedException {
+	@Test (priority=8)
+	public  void viewResume() throws Exception {
+		
+		EmployerLogin(EmploginID, EmpPWD);
 		hireBaseFunctions.Search();
 		WebElement ele=driver.findElement(By.linkText(p.getProperty("d_vwreslink")));
 		String val = ele.findElement(By.xpath("..")).getAttribute("id");
@@ -314,7 +416,53 @@ public class SearchResultPage extends hireBaseFunctions
 		System.out.println(actual);
 		String expected = "Resume Preview";
 		Assert.assertEquals(actual, expected);
+		
+		driver.findElement(By.linkText("Logout")).click();
+		
 	}
+	
+	@Test(priority=9)
+	public void ForwardResume() throws Exception 
+    {
+	
+	EmployerLogin(EmploginID, EmpPWD);
+	 
+	  Thread.sleep(1000);
+	  driver.findElement(By.xpath("//li[@id='maintab_searchCand']//a")).click();
+	driver.findElement(By.linkText("Quick Search")).click();
+	
+	driver.findElement(By.id("token-input-txtKeywordLeast")).click();
+	driver.findElement(By.id("token-input-txtKeywordLeast")).clear();
+	
+	//driver.findElement(By.id("token-input-txtKeywordLeast")).sendKeys( "\"jaiswal.simmi29@gmail.com\"" );
+	driver.findElement(By.id("token-input-txtKeywordLeast")).sendKeys("java");
+	
+	WebElement desg1=driver.findElement(By.id("token-input-txtKeywordLeast"));
+	Actions ajaxM1=new Actions(driver);
+	ajaxM1.moveToElement(desg1).build().perform();
+	
+	driver.findElement(By.xpath("//input[@class='blue-btn' and @type='submit']")).click();
+	
+	
+	WebElement ele = driver.findElement(By.linkText(p.getProperty("s_fwdlink")));
+	String val = ele.findElement(By.xpath("..")).getAttribute("id");
+	val= val.replaceAll("[^0-9]", "");
+	System.out.println(val);
+	driver.findElement(By.linkText(p.getProperty("s_fwdlink"))).click();
+	driver.findElement(By.id("fromEmailText"+val)).click();
+	driver.findElement(By.id("fromEmailText"+val)).sendKeys("testing@gmail.com");
+	driver.findElement(By.id("subjectEmailText"+val)).click();
+	driver.findElement(By.id("subjectEmailText"+val)).sendKeys("test message");
+	driver.findElement(By.id("message"+val)).click();
+	driver.findElement(By.id("message"+val)).sendKeys("hi this is test message"); 
+	driver.findElement(By.xpath("//div[@id='box_forwRes']//img[contains(@src,'images_corp/buttons/fwd_resume_submit_btn.gif')]")).click();
+	String actualmsg=driver.findElement(By.xpath("//div[@id='box_forwRes119308796']//div[@class='msgBoxSuccess']")).getText();
+	String expectedmsg="Mail Sent successfully";
+	org.testng.Assert.assertEquals(actualmsg, expectedmsg);
+	
+	Thread.sleep(2000);
+	driver.findElement(By.linkText("Logout")).click();
+    }
 	
 	@org.testng.annotations.AfterClass
 	public void teardown()
